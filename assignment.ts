76955381d169689204620e3b09ec14eda61c15c5
junaid110 <syedhoney0@gmail.com>
2023-07-15 17:48:40 +0500

    Add files via upload

---
+++ assignment.ts
@@ -0,0 +1,20 @@
+// Assignment 2
+
+// let fname:string = "Junaid"
+// let message = `Hello ${fname}, "Would you like to learn some typescript"`
+// console.log(message)
+
+// // Assignment 3
+
+// let pname:string = "Junaid Abbas";
+
+// console.log("Lower Case:  "+pname.toLowerCase());
+// console.log("Upper Case:  "+pname.toUpperCase());
+// console.log("Title Case:  " + pname.charAt(0).toUpperCase() + pname.slice(1).toLowerCase());
+
+// Assignment 4
+
+// let MyName:string = "Mir Taqi Mir"
+// let message = "It’s the beginning of Love, why do you wail; Just wait and watch how things unveil)"
+
+// console.log(`${MyName} "once Said" ${message}`)
